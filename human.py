d2dd817f11cd695a3c74bff629bc0f02f81450f6
신 종민 <standbyme227@algorithmlabs.co.kr>
2018-11-01 08:08:29 +0900

    first commit

---
+++ human.py
@@ -0,0 +1,9 @@
+class Human:
+    def __init__(self, id, weight, height, age,):
+        self.id = id
+        self.weight = weight
+        self.height = height
+        self.age = age
+
+    def do_nothing(self):
+        print()
